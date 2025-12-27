cc5a95ce8f34f93197f21604dcd0ac7ca75c8cb5
nrhdyt3012 <dayats354313@gmail.com>
2025-12-27 16:22:02 +0700

    build apk #3

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -17,11 +17,11 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
         }
     }
     compileOptions {
